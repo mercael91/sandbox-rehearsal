36f0b1374deb8144b7f5a8c47eed0e72e2775400
mercael <mercael91@gmail.com>
2026-08-21 15:22:21 +0300

    add tests/test_calculator.py

---
+++ tests/test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import add, divide, format_price, parse_int, average
+
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_divide_by_zero():
+    assert divide(5, 0) == "Cannot divide by zero"
+
+
+def test_average():
+    assert average([1, 2, 3]) == 2
